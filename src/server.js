79ddc55a41bb31aa5d554006e822ed4c0685fc0b
JoaoManoelFontes <jmanufontesc@gmail.com>
2022-02-12 08:04:03 -0300

    inicial express server commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = require('./routes/router');
+const connection = require('./database/connection');
+
+connection.authenticate().then(() => {
+    console.log("database conected")
+}).catch((err) => {
+    console.log(err);
+});
+
+const app = express();
+
+app.use(express.json())
+app.use('/users',router)
+
+app.listen(3333, () => {
+  console.log("server listening on port 3333!");
+});
