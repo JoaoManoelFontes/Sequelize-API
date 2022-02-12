44bc785e6e02be9ea5335d3e155af402b1c1468c
JoaoManoelFontes <jmanufontesc@gmail.com>
2022-02-12 08:07:09 -0300

    inicial database config commit

---
+++ src/database/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: 'mysql',
+    host:'localhost',
+    username:'YOUR_MYSQL_USERNAME',
+    password:'YOUR_MYSQL_PASSWORD',
+    database: 'YOUR_DATABASE_NAME',  
+    define:{    
+        timeStamps:true
+        }
+}
