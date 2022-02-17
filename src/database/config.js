6bc3d922073b8736a7bd254329176c0a90c6eb06
JoaoManoelFontes <jmanufontesc@gmail.com>
2022-02-16 22:11:19 -0300

    updated database configs

--- src/database/config.js
+++ src/database/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     dialect: process.env.DIALECT,
     host: process.env.HOST,
-    username: 'root',
+    username: process.env.ROOT,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,  
     define:{    
